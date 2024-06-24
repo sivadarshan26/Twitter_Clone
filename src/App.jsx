44b8ede907efef4a3e6292af5d7adf76782d9350
Sivadarshan <sivadarshan2270@gmail.com>
2024-06-24 11:51:46 +0530

    nearly completed the landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import Sidebar from './components/Sidebar';
 import Posts from './components/Posts';
+import Premium from './components/Premium'
 
 const App = () => {
   return (
-    <div className='flex h-screen overflow-hidden'>
+    <div className='flex h-screen overflow-hidden justify-center'>
       <Sidebar className='fixed top-0 left-0 w-1/4 h-full bg-gray-900 z-10 overflow-y-auto' />
-      <div className='flex flex-col ml-1/4 w-3/4 h-full overflow-y-auto'>
+      <div className='flex overflow-y-auto'>
         <Posts />
+        <Premium  className='sticky top-0'/>
+      </div>
+      <div className='overflow-y-auto'>
+      
       </div>
     </div>
   );
